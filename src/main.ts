461210c25e3a5cf119578fced9e4cc48232bbd6e
Osvaldo Aurélio Ribeiro Silva <osvaldo.aurelio@outlook.com>
2025-01-25 11:28:48 -0300

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app.module';
+
+const validationPipe = new ValidationPipe({
+  whitelist: true,
+  transform: true,
+});
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  
+  const config = app.get(ConfigService);
+
+  app.enableCors({
+    origin: config.get<string>('CORS_ORIGIN', '*'),
+    methods: config.get<string>('CORS_METHODS', 'GET,PUT,POST,DELETE'),
+  });
+  app.useGlobalPipes(validationPipe);
+  app.setGlobalPrefix(config.get<string>('GLOBAL_PREFIX', '/v1/api'));
+
+  await app.listen(config.get<number>('PORT', 3333));
+}
+
+bootstrap();
