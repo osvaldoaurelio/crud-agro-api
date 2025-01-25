4b278d401c940da866ad1109ce075f6ceea493d1
Osvaldo Aurélio Ribeiro Silva <osvaldo.aurelio@outlook.com>
2025-01-25 11:47:02 -0300

    add prisma service

---
+++ src/common/modules/prisma/prisma.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, OnModuleDestroy, OnModuleInit, Scope } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable({ scope: Scope.DEFAULT })
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor(readonly config: ConfigService) {
+    super({
+      datasources: {
+        db: { url: config.get<string>('DATABASE_URL') },
+      },
+      errorFormat: 'minimal',
+      log: ['query', 'info', 'warn'],
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+
+  cleanDb() {
+    return this.$transaction([
+      this.doctor.deleteMany(),
+    ]);
+  }
+}
